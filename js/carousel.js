2b806773d7e0fc78e79bb02e37673e64607a0faf
Remi <remihu@gmail.com>
2015-04-22 20:09:09 -0400

    finish carousel

--- js/carousel.js
+++ js/carousel.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	    slideSpeed = 400;
 
 	//set the width of div#tray
-	$("#tray").width(slideWidth * numOfSlides);
+	$(".tray").width(slideWidth * numOfSlides);
 
 	$("a.next").click(function(event){
 
@@ -21,7 +21,7 @@ $(document).ready(function(){
 		//calculate distance to move
 		var distance = positionNum * slideWidth;
 		//animate the #tray
-		$("#tray").animate({
+		$(".tray").animate({
 			right: distance
 		}, slideSpeed);
 
@@ -41,11 +41,23 @@ $(document).ready(function(){
 		//calculate distance to move
 		var distance = positionNum * slideWidth;
 		//animate the #tray
-		$("#tray").animate({
+		$(".tray").animate({
 			right: distance
 		}, slideSpeed);
 
 		event.preventDefault();
 	});
+
+	//hide arrows initially
+	$("a.prev, a.next").hide();
+
+	//show arrows on hover
+	$(".frame").hover(function(){
+		$("a.prev, a.next").show();
+	}, function(){
+		$("a.prev, a.next").hide();
+	});
+
+
 
 });
